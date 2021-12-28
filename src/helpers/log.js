50ead60c72584b6a29f9f518661d75b23f27d824
NoahKr <noahkroon1997@gmail.com>
2021-12-28 12:27:22 +0100

    log in local time

--- src/helpers/log.js
+++ src/helpers/log.js
@@ -1,9 +1,10 @@
 import fs from 'fs';
 
 export function log(message, source) {
-    const prefix = new Date().toISOString();
+    const tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
+    const localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0, -1);
 
-    fs.appendFileSync('./.log', `[${source}] ${prefix}: ${message}\n`);
+    fs.appendFileSync('./.log', `[${source}] ${localISOTime}: ${message}\n`);
 }
 
 export function logError(error, source) {
